a222b31d8ac91186e53da9dc545ee8e45914e9d9
saattrupdan <saattrupdan@gmail.com>
2026-08-22 10:28:43 +0200

    feat: add configurable viz time axes

--- tests/test_viz_data.py
+++ tests/test_viz_data.py
@@ -45,6 +45,34 @@ class VizDataTest(unittest.TestCase):
         self.assertEqual(data["spectrum"], [1, 0, 0, 0, 5])
         json.dumps(data, allow_nan=False)
 
+    def test_irregular_pctimes_make_relative_and_absolute_axes(self):
+        with h5py.File("in-memory", "w", driver="core", backing_store=False) as h5:
+            h5.create_dataset("SPECdata/Intensities", data=np.zeros((3, 2)))
+            h5.create_dataset(
+                "SPECdata/PCTime",
+                data=[[1_700_000_000], [1_700_000_002], [1_700_000_009]],
+            )
+            h5.attrs["Single Spec Duration (ms)"] = [1000.0]
+            axes = ptrms.viz_x_axis_data(h5)
+
+        self.assertEqual(axes["relative"], [0.0, 2.0, 9.0])
+        self.assertEqual(axes["absolute"], [1700000000.0, 1700000002.0, 1700000009.0])
+        self.assertTrue(axes["absolute_available"])
+
+    def test_malformed_pctimes_disable_absolute_and_use_duration_fallback(self):
+        with h5py.File("in-memory", "w", driver="core", backing_store=False) as h5:
+            h5.create_dataset("SPECdata/Intensities", data=np.zeros((3, 2)))
+            h5.create_dataset(
+                "SPECdata/PCTime",
+                data=[[1_700_000_000], [np.nan], [1_700_000_009]],
+            )
+            h5.attrs["Single Spec Duration (ms)"] = [2500.0]
+            axes = ptrms.viz_x_axis_data(h5)
+
+        self.assertIsNone(axes["absolute"])
+        self.assertFalse(axes["absolute_available"])
+        self.assertEqual(axes["relative"], [0.0, 2.5, 5.0])
+
 
 if __name__ == "__main__":
     unittest.main()
